04f3fb88ec89e6cc3cb86f280a0c25d09554432f
MustafazadeNihad <144814106+MustafazadeNihad@users.noreply.github.com>
2026-03-05 17:17:04 +0300

    Add files via upload

---
+++ Lessons/JS Lesson/main.js
@@ -0,0 +1,31 @@
+// Premitive type
+// 1/1
+// var x=5
+// var y=4
+// var z=3
+// var X=x+y
+// var Y=y-z
+// var Z=X+Y
+// console.log(Z)
+
+// 1/2
+// var x="Mustafazada"
+// var y=" Nihad"
+// console.log(x+y)
+
+// References type 
+// 2/1
+// var telebe=[{ad: "Nihad", soyad: "Mustafazada", yas: 19},
+//     {ad: "Nihat", soyad: "Mahmudov", yas: 20},
+//     {ad: "Rafiqa", soyad: "Muradova", yas: 22}]
+//     console.log(telebe)
+//     console.log(typeof(telebe))
+
+// 2/2
+// var x=15
+// var y=25
+// var z=x+y
+// function nihad() {
+//     console.log(z)
+// }
+// nihad()
